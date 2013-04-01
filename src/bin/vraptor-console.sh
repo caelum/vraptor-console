d6b370939fba2828594438cab68ecba6d1229fde
guilherme e rodrigo e chico <guilherme.silveira@caelum.com.br>
2013-04-01 09:34:32 -0300

    vraptor console sh first attempt

--- src/bin/vraptor-console.sh
+++ src/bin/vraptor-console.sh
@@ -1,2 +1,9 @@
+BINARY=vraptor-console-0.7.0-SNAPSHOT-distribution.jar
+TARGET=target/release/$BINARY
+
 # http://docs.oracle.com/javase/7/docs/technotes/guides/jpda/conninv.html
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -jar vraptor-console-*.jar
+if [ ! -f $TARGET ];
+then
+	curl http://ibiblio.com/$BINARY > $TARGET
+fi
+java -Xmx2g -Xms1g -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -cp target/vraptor-console-extra -jar $TARGET $*
