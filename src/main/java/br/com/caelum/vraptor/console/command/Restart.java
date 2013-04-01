930f40ab5271f3a2e2d74648ff19bc00a65baf87
Francisco Sokol <chico.sokol@gmail.com>
2013-04-01 09:53:33 -0300

    throwing UnsupportedOperationException

--- src/main/java/br/com/caelum/vraptor/console/command/Restart.java
+++ src/main/java/br/com/caelum/vraptor/console/command/Restart.java
@@ -1,11 +1,6 @@
 package br.com.caelum.vraptor.console.command;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 
 public class Restart implements Command {
 
@@ -13,24 +8,17 @@ public class Restart implements Command {
 	public void execute() throws Exception {
 		new Maven().execute(new CommandLine("compile"));
 		if (new File("jetty").exists()) {
-			customNotImplementedJetty();
+			customJetty();
 		} else {
 			RunningServer.restart();
 		}
 	}
-	
-	private void customNotImplementedJetty() throws MalformedURLException,
-			ClassNotFoundException, NoSuchMethodException,
-			InstantiationException, IllegalAccessException,
-			InvocationTargetException {
-		// use esse diretorio
-		URLClassLoader loader = new URLClassLoader(new URL[] { new File(
-				"jetty").toURL() }, this.getClass().getClassLoader());
-		Class<?> type = loader.loadClass("br.....Main");
-		Method method = type.getMethod("main",
-				new Class[] { String[].class });
-		Object instance = type.newInstance();
-		method.invoke(instance, new String[] {});
+
+	private void customJetty() {
+		throw new UnsupportedOperationException("/jetty dir found, but we haven't implemented " +
+				"custom jetty servers support, contact the developers for more info");
+		
 	}
+	
 
 }
