9c0af17a3a30faa3231a2777bd07aaa5857a398e
Francisco Sokol <chico.sokol@gmail.com>
2013-04-22 10:52:46 -0300

    jetty now is provided dep and downloaded before execution

---
+++ scripts/deploy-to-acceptance.sh
@@ -0,0 +1,12 @@
+set -e
+
+PROJECT_HOME=src/acceptance/myproducts/
+RELEASE_PATH=$PROJECT_HOME/vraptor-console/release/
+
+mvn clean package
+
+mkdir -p $PROJECT_HOME
+mkdir -p $RELEASE_PATH
+
+cp target/vraptor-console-0.7.0-SNAPSHOT-distribution.jar $RELEASE_PATH
+cp src/bin/vraptor-console.sh $PROJECT_HOME/
--- src/bin/vraptor-console.sh
+++ src/bin/vraptor-console.sh
@@ -1,11 +1,19 @@
 BINARY=vraptor-console-0.7.0-SNAPSHOT-distribution.jar
-BINARY_DIR=vraptor-console/target/
-TARGET=$BINARY_DIR/$BINARY
-mkdir -p $BINARY_DIR 
+RELEASE_PATH=vraptor-console/release/
+TARGET=$RELEASE_PATH/$BINARY
+JETTY_DIST=$RELEASE_PATH/jetty-distribution/
+JETTY_URL="http://eclipse.c3sl.ufpr.br/jetty/stable-8/dist/jetty-distribution-8.1.10.v20130312.zip"
 
-# http://docs.oracle.com/javase/7/docs/technotes/guides/jpda/conninv.html
-if [ ! -f $TARGET ];
-then
+if [ ! -f $TARGET ]; then
 	curl http://ibiblio.com/$BINARY > $TARGET
 fi
-java ${VRAPTOR_CONSOLE_OPTS} -Xmx2g -Xms1g -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -cp target/vraptor-console-extra -jar $TARGET $*
+if [ ! -f $JETTY_DIST ]; then
+	curl $JETTY_URL > $RELEASE_PATH/jetty-dist.zip
+	unzip $RELEASE_PATH/jetty-dist.zip -d $RELEASE_PATH >/dev/null
+	mv $RELEASE_PATH/jetty-distribution-8.*/ $RELEASE_PATH/jetty-distribution 
+fi
+
+# http://docs.oracle.com/javase/7/docs/technotes/guides/jpda/conninv.html
+java -Xmx2g -Xms1g -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044\
+    -cp "${JETTY_DIST}/lib/*:${JETTY_DIST}/lib/jsp/*:$TARGET" \
+    br.com.caelum.vraptor.console.Main
