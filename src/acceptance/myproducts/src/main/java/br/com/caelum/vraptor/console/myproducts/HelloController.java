6bb60d7cbca6f340c667ff65380243f44f9b4e63
Francisco Sokol <chico.sokol@gmail.com>
2013-04-22 09:36:51 -0300

    putting list and using c:foreach

--- src/acceptance/myproducts/src/main/java/br/com/caelum/vraptor/console/myproducts/HelloController.java
+++ src/acceptance/myproducts/src/main/java/br/com/caelum/vraptor/console/myproducts/HelloController.java
@@ -1,14 +1,24 @@
 package br.com.caelum.vraptor.console.myproducts;
 
+import java.util.Arrays;
+
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 
 @Resource
 public class HelloController {
 	
+	private final Result result;
+
+	public HelloController(Result result) {
+		this.result = result;
+	}
+	
 	@Path("/")
 	public void index() {
 		System.out.println("Executing hello#index 50");
+		result.include("list", Arrays.asList("string1", "string2"));
 	}
 
 }
--- src/acceptance/myproducts/src/main/webapp/WEB-INF/jsp/hello/index.jsp
+++ src/acceptance/myproducts/src/main/webapp/WEB-INF/jsp/hello/index.jsp
@@ -1,3 +1,11 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+
 <html>
-Main page 2
+	<body>
+		Main page 2 ${list} <br>
+		<c:forEach var="s" items="${list}">
+			${s} <br>
+		</c:forEach>
+	</body>
 </html>
