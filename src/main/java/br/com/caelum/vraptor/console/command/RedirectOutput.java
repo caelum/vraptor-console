90d461788ddf8fa13156198979d32cac14f046c6
Francisco Sokol <chico.sokol@gmail.com>
2013-05-05 14:18:41 -0300

    printing output

--- src/main/java/br/com/caelum/vraptor/console/command/RedirectOutput.java
+++ src/main/java/br/com/caelum/vraptor/console/command/RedirectOutput.java
@@ -27,6 +27,7 @@ public class RedirectOutput implements Runnable {
 			FileWriter output = new FileWriter(target);
 			while(sc.hasNextLine()) {
 				String line = sc.nextLine();
+				System.out.println(line);
 				output.write(line + "\n");
 				output.flush();
 			}
